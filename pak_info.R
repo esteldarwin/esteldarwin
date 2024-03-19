c01a62c441e96955fcc2e8f007f0bd0d80339a11
esteldarwin <1264367268@qq.com>
2024-03-19 15:57:42 +0800

    commit

---
+++ pak_info.R
@@ -0,0 +1,14 @@
+
+library("tidyverse") #加载包
+
+help("tidyverse") #获得帮助文件
+
+browseVignettes("tidyverse")  #直接会跳转到tidyverse网页
+
+apropos("^tidyverse")         #展现tidyverse包含的各组分
+
+ls("package:tidyverse") #查看包中的函数和数据集
+
+apropos("tidy", mode = "function")#查询包中包含关键字的函数
+
+browseVignettes(package = "tidyverse") #查看包中的Vignettes
