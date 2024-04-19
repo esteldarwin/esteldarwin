9c66be2654c80afd50d58f664a41da3ee2ad44f1
esteldarwin <1264367268@qq.com>
2024-04-19 11:16:04 +0800

    commit

---
+++ 0419.R
@@ -0,0 +1,4 @@
+install.packages("terra")
+install.packages("sf")
+library(terra)
+library(sf)
---
+++ reticulate.R
@@ -0,0 +1,4 @@
+install.packages("reticulate")
+library(reticulate)
+reticulate::repl_python()
+reticulate::install_python(version = '<version>')
